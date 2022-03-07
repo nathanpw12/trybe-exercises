6295ad9c5cb41b722bcd71c97ba0a46e10ee4ee5
nathanpw12 <nathanvitor38@gmail.com>
2022-03-07 19:04:32 -0300

    Bloco 4

---
+++  trybe-exercises /Aulas/Blocos/4/Conteudo/if-else.js
@@ -0,0 +1,14 @@
+let nota = 20
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else { 
+    console.log("Você foi reprovada(o)");
+}
+
+//teste
+   
