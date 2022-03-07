03ce9cdccac11c751fbb6e0a40d5473879fe153c
nathanpw12 <nathanvitor38@gmail.com>
2022-03-07 20:30:56 -0300

    exercicio

---
+++  trybe-exercises /Aulas/Blocos/4/exercicio/exercicio.js
@@ -0,0 +1,26 @@
+// Requisito 1
+
+let a = 10;
+let b = 4;
+
+adicao = a + b;
+console.log(adicao)
+
+subtracao = a - b;
+console.log(subtracao)
+
+multiplicacao = a * b;
+console.log(multiplicacao)
+
+divisao = a / b;
+console.log(divisao)
+
+modulo = a % b;
+console.log(modulo)
+
+//Requisito 2 
+
+let c = 30;
+let d = 20;
+
+if 
