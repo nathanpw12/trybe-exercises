c793a57b91168c0d46dc2ee23f4f397c1e4d8d7c
Nathan Vitor <nathanvitor38@gmail.com>
2022-03-09 17:55:37 -0300

    dinamica ao vivo

---
+++  trybe-exercises /Aulas/Blocos/4/Dinamica Ao Vivo/script.js
@@ -0,0 +1,29 @@
+// 1. Faça um algoritmo que calcule a soma de 50 a 100 usando a estrutura “for” e retorne no formato:
+// A soma de 50 a 100 é: X.
+
+let valor = [50];
+let soma = 0;
+
+for (let index = 51; index <= 100; index += 1) {
+    valor.push(index);   
+}
+
+for (let index = 0; index < valor.length; index += 1) {
+    soma += valor[index]   
+}
+
+console.log("A soma de 50 a 100 é: " + soma)
+
+// 2. Crie um algoritmo que conte quantos números do intervalo entre 1 e 125 são divisíveis por 3.
+// Caso a quantidade seja igual a 50, exiba uma mensagem secreta a sua escolha.
+
+for (let index1 = 1; index <= 125; index += 1) {
+    array.push 
+}
+
+// 3. Crie um algoritmo que simula o jogo “pedra, papel e tesoura” levando em consideração que são apenas duas pessoas jogando e imprima o resultado no formato:
+// “Jogador 1 vence” ou “Empate” ou “Jogador 2 vence”.
+// 4. Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
+// Imprima no console seguindo o exemplo: “A pessoa é maior de idade”.
+// 5. Crie um algoritmo que recebe a idade de Marcella, Carlos e Camila e imprima quem é a mais nova no formato:
+// “Pessoa” é a mais nova.
