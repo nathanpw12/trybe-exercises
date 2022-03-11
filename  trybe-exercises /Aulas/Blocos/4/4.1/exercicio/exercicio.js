315f63261a40cdee0ab593b5eef23a1c4c8945ad
Nathan Vitor <nathanvitor38@gmail.com>
2022-03-10 22:34:46 -0300

    fixar-arrays

---  trybe-exercises /Aulas/Blocos/4/4.1/exercicio/exercicio.js
+++  trybe-exercises /Aulas/Blocos/4/4.1/exercicio/exercicio.js
@@ -1,26 +1,46 @@
-// Requisito 1
+// // Requisito 1
 
-let a = 10;
-let b = 4;
+// let a = 10;
+// let b = 4;
 
-adicao = a + b;
-console.log(adicao)
+// adicao = a + b;
+// console.log(adicao)
 
-subtracao = a - b;
-console.log(subtracao)
+// subtracao = a - b;
+// console.log(subtracao)
 
-multiplicacao = a * b;
-console.log(multiplicacao)
+// multiplicacao = a * b;
+// console.log(multiplicacao)
 
-divisao = a / b;
-console.log(divisao)
+// divisao = a / b;
+// console.log(divisao)
 
-modulo = a % b;
-console.log(modulo)
+// modulo = a % b;
+// console.log(modulo)
 
-//Requisito 2 
+//Requisito 2
 
-let c = 30;
-let d = 20;
+// let c = 72;
+// let d = 70;
 
-if 
+// if (c > d) {
+//   console.log("O maior numero é:", c);
+// } else if (d > c) {
+//   console.log("O maior numero é: ", d);
+// } else {
+//   console.log("Os numeros são iguais");
+// }
+
+// Requisito 3
+
+// let e = 220;
+// let f = 200;
+// let g = 112;
+
+// if (e > f && e > g) {
+//   console.log("O maior numero é: ", e);
+// } else if (f > e && f > g) {
+//   console.log("O maior numero é: ", f);
+// } else if (g > f && g > e) {
+//   console.log("O maior numero é: ", g);
+// } 
---  trybe-exercises /Aulas/Blocos/4/4.2/exercicio/script.js
+++  trybe-exercises /Aulas/Blocos/4/4.2/exercicio/script.js
@@ -12,9 +12,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // console.log(numbers[7]);
 // console.log(numbers[8]);
 // console.log(numbers[9]);
- 
-  // 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; //
 
-  for (sum of numbers) {
-    console.log(sum);
-  }
+// 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; //
+
+// for(i = 0; i <= numbers.length-1; i += 1){
+//   console.log(i)
+// }
+
+let sum = 0;
+
+for(let i in numbers){
+  sum += numbers[i];
+}
+console.log(sum);
+// 3- Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;//
