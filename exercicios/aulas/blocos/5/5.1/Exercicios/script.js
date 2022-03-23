fd36a2f274be4f3a9ff06d5cff2a83175d5291f3
Nathan Vitor <nathanvitor38@gmail.com>
2022-03-23 10:01:57 -0300

    exercicio

---
+++ exercicios/aulas/blocos/5/5.1/Exercicios/script.js
@@ -0,0 +1,22 @@
+// 1 - Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+
+document.querySelectorAll(".center-content p")[0].innerHTML =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex aliquam odit ipsa consequuntur animi voluptas sed est a eaque inventore eveniet architecto asperiores iure natus temporibus, deleniti veniam, autem corporis.";
+
+// 2- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109))
+
+document.querySelector(".main-content ").style.backgroundColor =
+  "rgb(76,164,109)";
+
+// 3-  Crie uma função que mude a cor do quadrado vermelho para branco.
+
+document.querySelector(".center-content").style.backgroundColor = "white";
+
+// 4-  Crie uma função que corrija o texto da tag <h1>.
+
+document.querySelector(".title").innerHTML = "Exercício 5.1 - Java Script";
+
+// 5- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+
+document.querySelectorAll(".center-content p")[0].style.setProperty.toUpperCase
+
