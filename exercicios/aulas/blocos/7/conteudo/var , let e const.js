ceb77f4699a59c5d4f209748e113e04347177edd
Nathan Vitor <nathanvitor38@gmail.com>
2022-04-14 12:06:19 -0300

    Fim: var, let e const.js

--- exercicios/aulas/blocos/7/conteudo/var , let e const.js
+++ exercicios/aulas/blocos/7/conteudo/var , let e const.js
@@ -16,6 +16,30 @@ userInfo();
 
 //                            Var
 
+// Hoisting é um comportamento padrão do JavaScript que acontece com variáveis declaradas com var . Esse comportamento move todas as declarações destas variáveis para o topo do escopo global, independentemente de onde a declaração foi feita.
+
+age = 20;
+
+var age;
+
+console.log(age); // 20
+
+// Mesmo atribuindo um valor à variável age antes mesmo de declará-la, o console.log conseguiu imprimir o seu valor, isso por causa do hoisting . É como se tivéssemos primeiro declarado a variável age e depois atribuído o valor 20.
+
+// Porém, apenas a declaração da variável é movida para o topo do escopo e não seu valor. No código abaixo podemos ver um exemplo disso. Ao atribuirmos um valor à variável age apenas após o console.log , recebemos undefined como resultado. 
+
+var age;
+
+console.log(age); // undefined
+
+age = 20;
+
+// As var podem ser redeclaradas sem nenhum erro , e isso pode gerar problemas se vcoce perceber:
+
+var meioDia = "12:00";
+var meioDia = "00:00";
+console.log(meioDia); // meioDia = 00:00
+
 // As var tem o problema de vazamento de escopo, em que se vc usar um if ou um for por exemplo dentro de uma funçao  e chamar a variavel fora do escopo deles , ocorrera um vazemnto de escopo da var
 // exemplo
 
@@ -56,9 +80,12 @@ const euzinho = {
 euzinho.qualidaes = "qualidades: não ter defeitos";
 console.log(euzinho);
 
+// Podemos aplicar a mesma ideia para const que contem arrays e adicionar novos valores ao array com .push
+
+// EXEMPLO VISUAL DE ESCOPO : https://s3.us-east-2.amazonaws.com/assets.app.betrybe.com/fundamentals/javascript-es6/images/escopos-0945c242e80b8eb512adb7c89a37c72d.gif
 
 // Enfim,  QUANDO USAR LET , VAR E CONST?
 
-// Var = NUNCA 
+// Var = NUNCA
 // let = SEMPRE, que precisar de uma variavel q seu valor possa ser reatribuido
 // Const = SEMPRE , que precisar de uma certa constancia , eu diaria :)
