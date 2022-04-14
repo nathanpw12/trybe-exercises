5ffbb29e6499457bb8445934e33f48d875342719
Nathan Vitor <nathanvitor38@gmail.com>
2022-04-14 12:12:02 -0300

    template literals.js

--- exercicios/aulas/blocos/7/conteudo/template literals.js
+++ exercicios/aulas/blocos/7/conteudo/template literals.js
@@ -1,1 +1,24 @@
-// Os template literals são importantes pois irão nos ajudar a construir strings mais bem escritas e concisas, ajudando na prevenção de erros. 
+// Os template literals são importantes pois irão nos ajudar a construir strings mais bem escritas e concisas, ajudando na prevenção de erros.  
+
+// template literals é mais uma feature do ES6 que nos permite criar strings complexas de forma mais fácil.
+
+// Em vez de concatenar string assim:
+
+const myName = 'Isabella';
+console.log('Hello' + ' ' + myName + '!');
+
+// Podemos concatenar assim:
+
+const myNames = 'Isabella';
+console.log(`Welcome ${myNames}!`);
+
+// A sintaxe do template literals pede para usarmos o sinal de crases no início e no final da frase, e variáveis dentro de um ${...} . Você também pode adicionar uma expressão dentro das chaves, como ${a + b} . Outra novidade é poder adicionar quebras de linha com o template literals sem a necessidade de concatená-las com o operador + e \n para trocar de linha. Execute o código abaixo para ver o resultado!
+
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
