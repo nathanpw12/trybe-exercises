f068c47e4259b20d122a10ba4da8007f0fa9aef4
Nathan Vitor <nathanvitor38@gmail.com>
2022-04-16 16:28:08 -0300

    describe.js

---
+++ exercicios/aulas/blocos/7/7.3/conteudo/4 -  describe.js
@@ -0,0 +1,18 @@
+//  A função describe cria um bloco para agrupar vários testes. Isso é útil para melhorar a organização dos seus testes. Você pode utilizar describe , por exemplo, para separar testes de funções diferentes em um mesmo arquivo, ou para agrupar testes relacionados dentro de uma função complexa que requer muitos testes. É possível aninhar blocos describe arbitrariamente. Dentro de cada bloco, qualquer declaração de variáveis ou funções é local a este bloco. Leia os exemplos na documentação do Jest para fixar.
+
+// https://jestjs.io/docs/pt-BR/api#describename-fn
+
+const myBeverage = {
+  delicious: true,
+  sour: false,
+};
+
+describe('my beverage', () => {
+  test('is delicious', () => {
+    expect(myBeverage.delicious).toBeTruthy();
+  });
+
+  test('is not sour', () => {
+    expect(myBeverage.sour).toBeFalsy();
+  });
+});
