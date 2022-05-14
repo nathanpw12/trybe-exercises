d0f2c5e77919c05ee921931935ba17af6a5ee748
nathanpw12 <nathanvitor38@gmail.com>
2022-05-14 17:29:44 -0300

    Exercicio 1 , 8.1

--- exercicios/aulas/blocos/8/8.1/exercicio/hof.js
+++ exercicios/aulas/blocos/8/8.1/exercicio/hof.js
@@ -1,15 +1,24 @@
 // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
 
+const criaEmail = (nomeCompleto) => {
+  return `${nomeCompleto.replace(" ", "_")}@trybe.com`;
+};
+
+function retornaObjPessoa(nomeCompleto) {
+  return {
+    nomeCompleto,
+    email: criaEmail(nomeCompleto),
+  };
+}
+
 const newEmployees = () => {
   const employees = {
-    id1: "", // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: "", // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: "", // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: retornaObjPessoa("Pedro Guerra"),
+    id2: retornaObjPessoa("Luiza Drumond"),
+    id3: retornaObjPessoa("Carla Paiva"),
   };
   return employees;
 };
 
-const employeesInfo = (name, email) => {
-  return { nomeCompleto: name, email: email };
-};
-  
+console.log(newEmployees());
+
